4ecf1b6f6d3abcd21d8601d6ebc39564ca95a5d6
Carlos Eduardo <eduardo-1942@hotmail.com>
2022-07-28 16:35:52 -0300

    a10-fixação

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const numeros = [9,2,8,1];
+const jogadores = ["Messi", "Neymar", "Ronaldo"];
+const arrayMisto = [7, "craques", true]
+console.log(numeros.length);
+console.log(jogadores.length);
+console.log(arrayMisto.length);
+console.log(numeros[0]);
+console.log(jogadores[1]);
+console.log(arrayMisto[2]);
+console.log(numeros.includes(8));
+console.log(arrayMisto.includes("7"));
